c152e28a674909c87da9a210a6b7af0ea8779439
762366562@qq.com <762366562@qq.com>
2019-05-24 15:12:19 +0800

    修改属性配置界面

--- SinTabWindow.h
+++ SinTabWindow.h
@@ -1,208 +1,95 @@
-#ifndef SINTABWINDOW_H
-#define  SINTABWINDOW_H
-
-#pragma once
+#ifndef SINTABWIDGET_H
+#define SINTABWIDGET_H
+#include <QTabWidget>
+#include <QWidget>
 #include <QMainWindow>
-#include <QSignalMapper>
-#include <QSplitter>
 #include <QTabBar>
 
+class SinTabWindow;
+class TabBar;
 
-class SinTabFrameWork;
-class SinTabContainer;
-
-class SinMainWindow : public QMainWindow
+class GhostWindow : public QWidget
 {
-  Q_OBJECT
+	Q_OBJECT
 
 public:
-  SinMainWindow(SinTabFrameWork* tabFramework);
-
-protected:
-  virtual void closeEvent(QCloseEvent* event);
-
-private:
-  enum LayoutType
-  {
-    NoneType,
-    WindowType,
-    SplitterType,
-    ContainerType,
-    TabType,
-  };
+	explicit GhostWindow(TabBar *tabbar, const QPoint& pos);
+	const QPoint offset() { return m_offset; }
+	const int index() { return m_index; }
+	void moveWithOffset(const QPoint&);
+	bool dragStarted(const QPoint&);
 
 private:
-  SinTabFrameWork* tabFramework;
-  QWidget* overlayWidget;
-  QWidget* overlayWidgetTab;
-  QWidget* focusTab;
-
-private slots:
-  void hideCurrent();
-
-private:
-  void setDropOverlayRect(const QRect& globalRect, const QRect& tabRect = QRect());
-  void writeLayout(QDataStream& stream);
-  void readLayout(QDataStream& stream);
-  void hideAllTabs();
-  int tabCount();
-
-  friend class SinTabContainer;
-  friend class SinTabBar;
-  friend class SinTabFrameWork;
-  friend class SinTabSplitter;
+	QPoint m_offset;
+	int m_index;
+	QPoint m_originPos;
 };
 
-class SinTabFrameWork : public SinMainWindow
+
+class TabBar : public QTabBar
 {
-  Q_OBJECT
+	Q_OBJECT
 
 public:
-  enum InsertPolicy
-  {
-    InsertFloating,
-    InsertOnTop,
-    Insert,
-    InsertLeft,
-    InsertRight,
-    InsertTop,
-    InsertBottom
-  };
-
-public:
-  SinTabFrameWork();
-  ~SinTabFrameWork();
-
-  void addTab(QWidget* widget, InsertPolicy insertPolicy = InsertFloating, QWidget* position = 0);
-  void moveTab(QWidget* widget, InsertPolicy insertPolicy = InsertFloating, QWidget* position = 0);
-  void removeTab(QWidget* widget);
-  void hideTab(QWidget* widget);
-  bool isVisible(QWidget* widget) const;
-
-  QAction* toggleViewAction(QWidget* widget);
-
-  void restoreLayout(const QByteArray& layout);
-  QByteArray saveLayout();
+	TabBar(QWidget *parent = 0);
+	~TabBar();
+	void mousePressEvent(QMouseEvent *);
+	void mouseMoveEvent(QMouseEvent *);
+	void mouseReleaseEvent(QMouseEvent *);
 
 protected:
-  virtual void closeEvent(QCloseEvent* event);
-  virtual void showEvent(QShowEvent* event);
-  virtual bool eventFilter(QObject* obj, QEvent* event);
-  virtual bool event(QEvent* event);
+	void createNewWindow(GhostWindow*);
+	void moveToWindow(SinTabWindow*, const QPoint&, GhostWindow*);
+	void tabRemoved(int index);
 
 private:
-  struct TabData
-  {
-    QWidget* widget;
-    QString objectName;
-    bool hidden;
-    QAction* action;
-  };
-
-private:
-  QList<SinMainWindow*> floatingWindows;
-  QList<SinMainWindow*> floatingWindowsZOrder;
-  QList<SinMainWindow*> floatingWindowsZOrderToRestore;
-  QHash<QWidget*, TabData> tabs;
-  QHash<QString, TabData*> tabsByName;
-  QSignalMapper signalMapper;
-
-  QWidget* moveTabWidget;
-  SinTabContainer* moveTabPosition;
-  InsertPolicy moveTabInsertPolicy;
-  int moveTabTabIndex;
-
-private slots:
-  void executeMoveTab();
-  void toggleVisibility(QWidget* widget);
-  void showFloatingWindows();
-  void handleFocusChanged(QWidget* old, QWidget* now);
-
-private:
-  void addTab(QWidget* widget, SinTabContainer* position, InsertPolicy insertPolicy, int tabIndex);
-  void moveTab(QWidget* widget, SinTabContainer* position, InsertPolicy insertPolicy, int tabIndex);
-  void moveTabLater(QWidget* widget, SinTabContainer* position, InsertPolicy insertPolicy, int tabIndex);
-  void removeContainerIfEmpty(SinTabContainer* tabContainer);
-  SinMainWindow* createWindow();
-  void removeWindow(SinMainWindow* window);
-  void hideTab(QWidget* widget, bool removeContainerIfEmpty);
-  QString tabObjectName(QWidget* widget);
-  void unhideTab(const QString& objectName, SinTabContainer* position);
-
-  friend class SinTabBar;
-  friend class SinTabContainer;
-  friend class SinMainWindow;
+	GhostWindow *m_ghost;
 };
 
-class SinTabSplitter : public QSplitter
+
+class TabView : public QTabWidget
 {
+	Q_OBJECT
+
 public:
-  SinTabSplitter(Qt::Orientation orientation, QWidget* parent, SinMainWindow* tabWindow);
-
-private:
-  SinMainWindow* tabWindow;
-
-private:
-  void writeLayout(QDataStream& stream);
-  void readLayout(QDataStream& stream);
-
-  friend class SinMainWindow;
+	explicit TabView(QWidget *parent = 0);
+	int tabAt(const QPoint &pos);
 };
 
-class SinTabContainer : public QTabWidget
+class TabbedWindowPrivate : public QObject
 {
-  Q_OBJECT
+	Q_OBJECT
 
 public:
-  SinTabContainer(QWidget* parent, SinMainWindow* tabWindow);
+	TabbedWindowPrivate(SinTabWindow* q_ptr);
 
-  int addTab(QWidget* widget, const QString& label);
-  int insertTab(int index, QWidget* widget, const QString& label);
-  void removeTab(int index);
-
-protected:
-  virtual void dragEnterEvent(QDragEnterEvent* event);
-  virtual void dragLeaveEvent(QDragLeaveEvent* event);
-  virtual void dragMoveEvent(QDragMoveEvent* event);
-  virtual void dropEvent(QDropEvent* event);
+	int addView(QWidget* view, const QString &title);
+	int insertView(const QPoint &pos, QWidget *page, const QString &text);
+	void removeView(int index);
+	void setCurrentView(int index);
+	QWidget* currentView();
 
 private:
-  SinMainWindow* tabWindow;
-
-private slots:
-  void handleCurrentChanged(int index);
-
-private:
-  QRect findDropRect(const QPoint& globalPos, int tabWidth, SinTabFrameWork::InsertPolicy& insertPolicy, QRect& tabRect, int& tabIndex);
-
-  void writeLayout(QDataStream& stream);
-  void readLayout(QDataStream& stream);
-
-  friend class SinTabBar;
-  friend class SinTabFrameWork;
-  friend class SinMainWindow;
-  friend class SinTabSplitter;
+	SinTabWindow *q_ptr;
+	TabView* tabs;
 };
 
-class SinTabBar : public QTabBar
+class SinTabWindow:public QMainWindow
 {
-  Q_OBJECT
+	Q_OBJECT
 
 public:
-  SinTabBar(SinTabContainer* tabContainer);
+	explicit SinTabWindow(QWidget *parent = 0);
+	~SinTabWindow();
 
-protected:
-  virtual void mousePressEvent(QMouseEvent* event);
-  virtual void mouseReleaseEvent(QMouseEvent* event);
-  virtual void mouseMoveEvent(QMouseEvent* event);
+	int addView(QWidget* view, const QString &title);
+	int insertView(const QPoint &pos, QWidget *page, const QString &text);
+	void removeView(int index);
+	void setCurrentView(int index);
+	QWidget* currentView();
 
 private:
-  SinTabContainer* tabContainer;
-  int pressedIndex;
-  QPoint dragStartPosition;
-
-private slots:
-  void closeTab(int index);
+	TabbedWindowPrivate* d_ptr;
 };
 
 #endif
